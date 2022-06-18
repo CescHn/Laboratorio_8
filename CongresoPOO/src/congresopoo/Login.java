c83cda844652c18adff851af305a120ac44d82bb
D-Zzavala <65042896+D-Zzavala@users.noreply.github.com>
2022-06-17 23:04:06 -0600

    menu modificar y CSJ modificar es el hola

--- CongresoPOO/src/congresopoo/Login.java
+++ CongresoPOO/src/congresopoo/Login.java
@@ -54,6 +54,8 @@ public class Login extends javax.swing.JFrame {
         }
     }
 
+    
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
